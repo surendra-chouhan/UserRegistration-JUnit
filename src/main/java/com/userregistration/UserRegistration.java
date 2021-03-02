7917a1cd0fe5adca7b32763f50d729f0c010ca79
surendra-chouhan <chouhansurendra028@gmail.com>
2021-03-02 21:46:57 +0530

    Added method to check for valid LastName in UserRegistrationTest.java

--- src/main/java/com/userregistration/UserRegistration.java
+++ src/main/java/com/userregistration/UserRegistration.java
@@ -4,10 +4,16 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
     public boolean result;
-    private static final String patternForFirstName = "[A-Z]{1}[a-z]{2,}";
+    private static final String patternForName = "[A-Z]{1}[a-z]{2,}";
 
     public boolean FirstName(String FirstName) {
-        result = Pattern.compile(patternForFirstName).matcher(FirstName).matches();
+        result = Pattern.compile(patternForName).matcher(FirstName).matches();
         return result;
     }
+
+    public boolean LastName(String LastName){
+        result = Pattern.compile(patternForName).matcher(LastName).matches();
+        return result;
+    }
+
 }
--- src/test/java/com/userregistration/UserRegistrationTest.java
+++ src/test/java/com/userregistration/UserRegistrationTest.java
@@ -7,16 +7,30 @@ import org.junit.Test;
 public class UserRegistrationTest {
 
     @Test
-    public void give_FirstName_True_test() {
+    public void give_FirstName_True_Test() {
         UserRegistration user = new UserRegistration();
-        boolean result = user.FirstName("surendra");
+        boolean result = user.FirstName("Surendra");
         Assert.assertTrue(result);
     }
 
     @Test
-    public void give_FirstName_Flase_Test() {
+    public void give_FirstName_False_Test() {
         UserRegistration user = new UserRegistration();
         boolean result = user.FirstName("surendra");
+        Assert.assertFalse(result);
+    }
+
+    @Test
+    public void give_LastName_True_Test() {
+        UserRegistration user = new UserRegistration();
+        boolean result = user.LastName("Chouhan");
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void give_LastName_False_Test() {
+        UserRegistration user = new UserRegistration();
+        boolean result = user.LastName("chouhan");
         Assert.assertFalse(result);
     }
 }
